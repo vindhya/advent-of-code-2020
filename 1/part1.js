304cdc78ccc49ac3e4305a6df1c41e6be2527812
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2020-12-01 20:06:36 -0500

    day 1 initial solution

---
+++ 1/part1.js
@@ -0,0 +1,18 @@
+const numbers = require('./input');
+
+let num1, num2;
+
+numbers.sort((a, b) => a - b);
+
+for (let i = 0; i < numbers.length; i++) {
+  for (let j = i + 1; j < numbers.length; j++) {
+    if (numbers[i] + numbers[j] === 2020) {
+      num1 = numbers[i];
+      num2 = numbers[j];
+      break;
+    }
+  }
+}
+
+console.log(num1, num2);
+console.log(num1 * num2);
---
+++ 1/part2.js
@@ -0,0 +1,21 @@
+const numbers = require('./input');
+// const numbers = [1721, 979, 366, 299, 675, 1456];
+
+numbers.sort((a, b) => a - b);
+
+let num1, num2, num3;
+
+for (let i = 0; i < numbers.length; i++) {
+  for (let j = i + 1; j < numbers.length; j++) {
+    for (let k = i + 2; k < numbers.length; k++) {
+      if (numbers[i] + numbers[j] + numbers[k] === 2020) {
+        num1 = numbers[i];
+        num2 = numbers[j];
+        num3 = numbers[k];
+      }
+    }
+  }
+}
+
+console.log(num1, num2, num3);
+console.log(num1 * num2 * num3);
